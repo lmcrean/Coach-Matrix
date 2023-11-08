dfe107ae8c759a1d3e83684d114c01f7ab8057b9
Laurie Crean <lmcrean@gmail.com>
2023-11-08 20:12:45 +0000

    Comments are working

--- codestar/settings.py
+++ codestar/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog'
 ]
 
@@ -62,6 +63,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
