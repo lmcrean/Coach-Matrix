40c700153137ee2e1beef71080cdf5a9cc2f531c
Laurie Crean <lmcrean@gmail.com>
2023-11-10 19:15:08 +0000

    urls and views for users

--- codestar/urls.py
+++ codestar/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path("", include("googleauthentication.urls")),
     path("accounts/", include("allauth.urls")),
+    path("/", include("users.urls"))
 ]
