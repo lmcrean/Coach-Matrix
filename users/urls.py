3d6f7b0c4ea9ad2170ae469332af86d5e0ffa56c
Laurie Crean <lmcrean@gmail.com>
2023-12-28 16:36:57 +0000

    logo click when signed out leads to landing page (not questions)

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
+#users/urls.py
+
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.home), # home page
+    path("", views.home, name='home'), # home page
     path("questions/", views.logintoquestions, name='questions'), # questions page
     path("accounts/", include("allauth.urls")),
     path("logout", views.logout_view)
