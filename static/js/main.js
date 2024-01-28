8fc4ff949f4ee823ef5b005b5eea2dd182e00fa5
Laurie Crean <lmcrean@gmail.com>
2024-01-28 14:30:03 +0000

    enable all css and js files except for navbar.css

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 // Load all the js files
 
-// import './ask_question.js';
-// import './bg-logo.js';
-// import './login.js';
-// import './navbar.js';
+import './ask_question.js';
+import './bg-logo.js';
+import './login.js';
+import './navbar.js';
