25807a9fe35e77cda35b59d978bc1e24aadb682c
Laurie Crean <lmcrean@gmail.com>
2024-02-16 16:48:05 +0000

    Add TaggedItem model to filters.py and update filtered_questions.html to include tags

--- main_forum/views/filters.py
+++ main_forum/views/filters.py
@@ -4,7 +4,7 @@
 from django.views.generic.list import ListView
 from ..models import Question
 from django.shortcuts import get_object_or_404
-from taggit.models import Tag
+from taggit.models import Tag, TaggedItem
 
 class FilterByTagView(ListView):
     model = Question
@@ -13,13 +13,15 @@ class FilterByTagView(ListView):
 
     def get_queryset(self):
         tag_slug = self.kwargs.get('tag_slug')
-        # Retrieve the Tag object by slug
         tag = get_object_or_404(Tag, slug=tag_slug)
-        # Use TaggedItemManager's filter method to get questions tagged with the retrieved tag
-        return Question.objects.filter(tags__slug=tag.slug)
+        # Use TaggedItem to query all items associated with the tag
+        questions_ids = TaggedItem.objects.filter(tag_id=tag.id).values_list('object_id', flat=True)
+        print(questions_ids)
+        return Question.objects.filter(id__in=questions_ids)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         tag_slug = self.kwargs.get('tag_slug')
         context['tag'] = get_object_or_404(Tag, slug=tag_slug)
         return context
+
