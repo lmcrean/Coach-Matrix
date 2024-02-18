5f23240029fc231f4bf43f89a65f45147581614e
Laurie Crean <lmcrean@gmail.com>
2024-02-18 14:44:36 +0000

    Refactor profile form templates and forms.py for profile updating, in process of compressing profile functionality into a class based view

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path
 from .views import (
-    home, custom_login_view, logout_view, questions_view, custom_signup_view, change_password, profile_view
+    home, custom_login_view, logout_view, questions_view, custom_signup_view, change_password, profile_view, profile_update
     )
 
 urlpatterns = [
@@ -10,8 +10,5 @@ urlpatterns = [
     path('login/', custom_login_view, name='custom_login'),
     path('signup/', custom_signup_view, name='custom_signup'),
     path('logout/', logout_view, name='logout'),
-    path('profile/', profile_view, name='my_profile'),
-    path('profile/', profile_view, name='profile_update'),
-    path('profile/', change_password, name='change_password'),
-    path('profile/', profile_view, name='change_password_done'),
+    path('profile/', profile_view, name='my_profile')
 ]
