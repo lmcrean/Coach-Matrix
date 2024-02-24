44b677a772e85612b6fbcbe97cf412f8c030458c
Laurie Crean <lmcrean@gmail.com>
2024-02-24 15:37:55 +0000

    Add question_tags to check if a question is bookmarked by a user

---
+++ main_forum/templatetags/question_tags.py
@@ -0,0 +1,12 @@
+# main_forum/templatetags/question_tags.py
+
+# this is a custom template tag that will be used to check if a question is bookmarked by a user.
+
+from django import template
+from main_forum.models import Bookmark
+
+register = template.Library()
+
+@register.filter
+def is_bookmarked_by(question, user):
+    return Bookmark.objects.filter(question=question, user=user).exists()
