8f60016e031c36754b04f77c9a3888b33cee46ff
Laurie Crean <lmcrean@gmail.com>
2024-03-10 11:31:44 +0000

    Add get_reputation method to UserProfile model

--- main_forum/models/user_profile_model.py
+++ main_forum/models/user_profile_model.py
@@ -10,10 +10,24 @@ from django_quill.fields import QuillField
 from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 from taggit.managers import TaggableManager
+from .reputation_points_model import ReputationPoints
 
 class UserProfile(models.Model):
+    """
+    This stores additional information to the Django User model, including the user's reputation points. The reputation points are calculated based on the user's upvotes and downvotes.
+
+    This should retrieve and attribute the user's reputation points from the main_forum/models/reputation_points_model.py.
+
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    reputation = models.IntegerField(default=0)
+
+
+    def get_reputation(self):
+        """
+        This method retrieves the user's reputation points from the main_forum/models/reputation_points_model.py. It's useful for getting the user's reputation points in the template, and centralising the data.
+        """
+        reputation_points_instance, created = ReputationPoints.objects.get_or_create(user=self.user)
+        return reputation_points_instance.reputation
 
     def __str__(self):
         return self.user.username
