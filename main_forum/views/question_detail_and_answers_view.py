0794ca5e191d2f2567a054b3f0fa81659ca2f094
Laurie Crean <lmcrean@gmail.com>
2024-03-17 14:34:45 +0000

    Add debug print statements to QuestionDetail class

--- main_forum/views/question_detail_and_answers_view.py
+++ main_forum/views/question_detail_and_answers_view.py
@@ -1,3 +1,5 @@
+#main_forum/views/question_detail_and_answers_view
+
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import Count
 from django.http import HttpResponseRedirect
@@ -37,18 +39,22 @@ class QuestionDetail(View):
         """
         Post a new answer to the question.
         """
+        print("About to validate form") #prints as expected
+        print("request.POST",request.POST) # PASS: prints <QueryDict: {'csrfmiddlewaretoken': ['uHvvhe4O8rE0vaCV7eAi8gqIqi2O0BNtmQm2XRWIB8lgz0PKSidGeP1dKxB6zU4g'], 'body': ['']}>
         question = self.get_question(slug)
         form = AnswerForm(data=request.POST)
 
         if form.is_valid():
             # if the form is valid, save the answer and redirect to the question detail page 
+            print('valid form')
             self.save_answer(form, question, request.user)
             return redirect('question_detail', slug=slug)
         else:
+            print('form is not valid') # prints when expected
             answers, _ = self.get_sorted_answers(question)
             for field, errors in form.errors.items():
                 for error in errors:
-                    print(f"{error}")
+                    print(f"{field}: {error}") # prints "body: This field is required. Was expecting a more specific error message.
             return render(request, 'question_detail.html', {
                 'question': question,
                 'answers': answers,
