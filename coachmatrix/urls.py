0c708fb8d344a14e7f66e39bd4813fd65ece51bc
Laurie Crean <lmcrean@gmail.com>
2024-03-17 16:10:23 +0000

    Validate AnswerForm according to PEP8

--- coachmatrix/urls.py
+++ coachmatrix/urls.py
@@ -1,13 +1,14 @@
-# coachmatrix/urls.py 
-# this is the main URL configuration for the project. It is the first thing that is read when the server is started. It is responsible for routing requests to the appropriate app.
+# coachmatrix/urls.py
+# this is the main URL configuration for the project.
+# It is the first thing that is read when the server is started.
+# It is responsible for routing requests to the appropriate app.
 
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),  # Include the allauth app URLs
-    path('', include('users.urls')),  # Include the users app URLs at the root
-    path('', include('main_forum.urls')), 
+    path('accounts/', include('allauth.urls')),
+    path('', include('users.urls')),
+    path('', include('main_forum.urls')),
 ]
--- main_forum/forms/answer_form.py
+++ main_forum/forms/answer_form.py
@@ -1,7 +1,5 @@
 # main_forum/forms/answer_form.py
 
-# This file will contain the form for the Answer model. The AnswerForm class will be used to create and update answers to questions.
-
 from better_profanity import profanity
 from django import forms
 from django.contrib.auth.models import User
@@ -18,6 +16,7 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.core.exceptions import ValidationError
 from django.utils.html import strip_tags
 
+
 def normalize_html(html_content):
     """
     Normalize HTML content by removing HTML tags and normalizing whitespace.
@@ -26,9 +25,13 @@ def normalize_html(html_content):
     normalized_content = ' '.join(text_content.split())
     return normalized_content
 
+
 class AnswerForm(forms.ModelForm):
     """
-    Form for answering a question as seen in question_detail.html. The user can enter the main body of the answer with django_quill module. If the user is updating an answer, the form needs to be pre-populated with the existing data.
+    Form for answering a question as seen in question_detail.html.
+    The user can enter the main body of the answer with django_quill module.
+    If the user is updating an answer, the form needs to be pre-populated with
+    the existing data.
     """
     body = QuillFormField(
         validators=[
@@ -37,7 +40,8 @@ class AnswerForm(forms.ModelForm):
         ],
         help_text='Enter the main body of your answer. 50 to 5000 characters.'
     )
-    class Meta: 
+
+    class Meta:
         """
         Meta class is used to specify the model to which the form is associated
         """
@@ -46,7 +50,8 @@ class AnswerForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         """
-        This is for checking if the form is bound to an existing instance, i.e. if the form is being used to update an existing answer.
+        This is for checking if the form is bound to an existing instance,
+        i.e. if the form is being used to update an existing answer.
 
         1. get the request from the kwargs
         2. call the parent __init__ method
@@ -55,11 +60,12 @@ class AnswerForm(forms.ModelForm):
         super(AnswerForm, self).__init__(*args, **kwargs)
 
     def clean(self):
-        super().clean() # Call the parent clean method
+        super().clean()  # Call the parent clean method
 
     def clean_body(self):
         """
-        This method is used to validate the body field in further detail from the initial class.
+        This method is used to validate the body field in further detail from
+        the initial class.
 
         1. get the body data from the cleaned data
         2. parse the JSON string to extract the HTML content
@@ -76,37 +82,67 @@ class AnswerForm(forms.ModelForm):
             html_content = body_json.get('html', '')
         except json.JSONDecodeError:
             # Handle the error if the body_data is not a valid JSON string
-            messages.error(self.request, "Invalid input format. Please ensure your input is correctly formatted.")
-            raise ValidationError("Invalid input format. Please ensure your input is correctly formatted.")
+            messages.error(
+                self.request,
+                "Invalid input format. Please ensure your input is"
+                "correctly formatted."
+                )
+            raise ValidationError(
+                "Invalid input format. Please ensure your input is correctly"
+                "formatted."
+                )
 
         if re.search(r'<br>.*<br>.*<br>', html_content):
-            messages.error(self.request, "Please remove any extra new lines from the content of your answer.")
-            raise ValidationError("Please remove any extra new lines from the content of your answer.")
+            messages.error(
+                self.request,
+                "Please remove any extra new lines from the content of your"
+                "answer.")
+            raise ValidationError(
+                "Please remove any extra new lines from the content of your"
+                "answer."
+                )
         if re.search(r' {3,}', html_content):
-            messages.error(self.request, "Please remove any extra spaces from the content of your answer.")
-            raise ValidationError("Please remove any extra spaces from the content of your answer.")
+            messages.error(
+                self.request,
+                "Please remove any extra spaces from the content of your"
+                "answer."
+                )
+            raise ValidationError(
+                "Please remove any extra spaces from the content of your"
+                "answer."
+                )
 
-        if re.search(r' {3,}', html_content):
-            messages.error(self.request, "Please remove any extra spaces from the content of your answer.")
-            raise ValidationError("Please remove any extra spaces from the content of your answer.")
-        
         if profanity.contains_profanity(html_content):
-            messages.error(self.request, "Please remove any profanity from the content of your answer.")
-            raise ValidationError("Please remove any profanity from the content of your answer.")
+            messages.error(
+                self.request,
+                "Please remove any profanity from the content of your answer.")
+            raise ValidationError(
+                "Please remove any profanity from the content of your answer.")
 
         if len(html_content) < 50:
-            messages.error(self.request, "Your answer must be at least 50 characters long.")
-            raise ValidationError("Your answer must be at least 50 characters long.")
+            messages.error(
+                self.request,
+                "Your answer must be at least 50 characters long.")
+            raise ValidationError(
+                "Your answer must be at least 50 characters long.")
         if len(html_content) > 5000:
-            messages.error(self.request, "Your answer must be no more than 5000 characters long.")
-            raise ValidationError("Your answer must be no more than 5000 characters long.")
+            messages.error(
+                self.request,
+                "Your answer must be no more than 5000 characters long."
+                    )
+            raise ValidationError(
+                "Your answer must be no more than 5000characters long.")
 
         normalized_html_content = normalize_html(html_content)
         query = Answer.objects.filter(body__icontains=normalized_html_content)
-        if self.instance.pk: # If the instance is being updated
-            query = query.exclude(pk=self.instance.pk) # Exclude the current answer if it is being updated
+        if self.instance.pk:  # If the instance is being updated
+            # Exclude the current answer if it is being updated
+            query = query.exclude(pk=self.instance.pk)
         if query.exists():
-            messages.error(self.request, "This answer already exists. Do not copy and paste duplicate answers.")
+            messages.error(
+                self.request,
+                "This answer already exists."
+                "Do not copy and paste duplicate answers.")
             raise ValidationError("This answer already exists.")
 
         return body_data
