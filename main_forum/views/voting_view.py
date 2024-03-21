80d8c2aa3470b1fd5fc849d70e9ff4d8429f622d
Laurie Crean <lmcrean@gmail.com>
2024-03-21 15:25:33 +0000

    Validation with pep8 for voting_view continued

--- main_forum/views/voting_view.py
+++ main_forum/views/voting_view.py
@@ -86,7 +86,7 @@ class BaseVotingView(LoginRequiredMixin, View):
 
         if opposite_vote_attr.filter(id=request.user.id).exists():
             messages.error(
-                request, 
+                request,
                 "Please remove your existing vote "
                 "before voting in the opposite direction."
             )
@@ -96,11 +96,10 @@ class BaseVotingView(LoginRequiredMixin, View):
             vote_attr.remove(request.user)
             messages.success(request, "Your vote has been removed.")
         else:
-            vote_attr.add(request.user) # add the vote if it does not exist
+            vote_attr.add(request.user)  # add the vote if it does not exist
             messages.success(request, "Your vote has been added.")
 
         return self.get_redirect_url(obj, origin_page)
-
 
     def get_redirect_url(self, obj, origin_page):
         """
@@ -113,76 +112,114 @@ class BaseVotingView(LoginRequiredMixin, View):
             1. If the referring page is the filtered questions page, the method
             redirects the user to the filtered questions page with the same tag
             1.1. If the referring page is the filtered questions page, and the
-            user has toggled on most recent votes, the method redirects the user
-            to the filtered questions page with the same tag and most recent votes.
-            1.2 If the referring page is the filtered questions page, and the user has toggled on most votes, same as above, but for most votes.
+            user has toggled on most recent votes, the method redirects the
+            user to the filtered questions page with the same tag and most
+            recent votes.
+            1.2 If the referring page is the filtered questions page, and the
+            user has toggled on most votes, same as above, but for most votes.
 
-            2. If the referring page is the question list page, the method redirects the user to the question list page.
-            2.1 If the referring page is the question list page, and the user has toggled on most recent votes, the method redirects the user to the question list page with most recent votes.
-            2.2 If the referring page is the question list page, and the user has toggled on most votes, same as above, but for most votes.
+            2. If the referring page is the question list page, the method
+            redirects the user to the question list page.
+            2.1 If the referring page is the question list page, and the user
+            has toggled on most recent votes, the method redirects the user to
+            the question list page with most recent votes.
+            2.2 If the referring page is the question list page, and the user
+            has toggled on most votes, same as above, but for most votes.
 
-            3. If the referring page is the question detail page, the method redirects the user to the question detail page for the question that was voted on.
+            3. If the referring page is the question detail page, the method
+            redirects the user to the question detail page for the question
+            that was voted on.
 
-        if the object is an answer, the method redirects the user to the question detail page for the question that the answer belongs to.
+        if the object is an answer, the method redirects the user to the
+        question detail page for the question that the answer belongs to.
         """
-        filtered_questions_pattern = r'/questions/tag/(?P<slug>[\w-]+)/$' # test pass
-        filtered_questions_pattern_most_recent = r'/questions/tag/(?P<slug>[\w-]+)/\?sort_by=recent' # testing fails, expecting to reach an url such as http://coachmatrix.org/questions/tag/health/?sort_by=recent
-        filtered_questions_pattern_most_votes = r'/questions/tag/(?P<slug>[\w-]+)/\?sort_by=votes' # testing fails
+        filtered_questions_pattern = r'/questions/tag/(?P<slug>[\w-]+)/$'
+        filtered_questions_pattern_most_recent = (
+            r'/questions/tag/(?P<slug>[\w-]+)/\?sort_by=recent')
+        filtered_questions_pattern_most_votes = (
+            r'/questions/tag/(?P<slug>[\w-]+)/\?sort_by=votes')
 
-        question_list_pattern = r'/questions/$' # test pass
-        question_list_pattern_most_recent = r'/questions/\?sort_by=recent' # testing passes
-        question_list_pattern_most_votes = r'/questions/\?sort_by=votes' # testing passes
+        question_list_pattern = r'/questions/$'
+        question_list_pattern_most_recent = r'/questions/\?sort_by=recent'
+        question_list_pattern_most_votes = r'/questions/\?sort_by=votes'
 
-        question_detail_pattern = r'/(?P<slug>[\w-]+)/$' # test pass
-        
+        question_detail_pattern = r'/(?P<slug>[\w-]+)/$'
+
         if isinstance(obj, Question):
-            # Check for specific filtered questions patterns -- testing fails when voting from an url such as http://coachmatrix.org/questions/tag/tag2/?sort_by=recent, returns to the question list view
+            """Check for specific filtered questions patterns"""
             if re.search(filtered_questions_pattern_most_recent, origin_page):
-                tag_slug = re.search(filtered_questions_pattern_most_recent, origin_page).group('slug')
-                return HttpResponseRedirect(reverse('filter_by_tag', kwargs={'tag_slug': tag_slug}) + '?sort_by=recent')
+                tag_slug = (
+                    re.search(
+                        filtered_questions_pattern_most_recent, origin_page)
+                    .group('slug')
+                    )
+                return HttpResponseRedirect(
+                    reverse(
+                        'filter_by_tag',
+                        kwargs={'tag_slug': tag_slug}
+                    ) + '?sort_by=recent'
+                )
             elif re.search(filtered_questions_pattern_most_votes, origin_page):
-                tag_slug = re.search(filtered_questions_pattern_most_votes, origin_page).group('slug')
-                return HttpResponseRedirect(reverse('filter_by_tag', kwargs={'tag_slug': tag_slug}) + '?sort_by=votes')
-            
-            # Check for question list patterns with sort parameters -- testing passes
+                tag_slug = re.search(
+                    filtered_questions_pattern_most_votes,
+                    origin_page).group('slug')
+                return HttpResponseRedirect(
+                    reverse('filter_by_tag',
+                    kwargs={'tag_slug': tag_slug}) + '?sort_by=votes'
+                )
             elif re.search(question_list_pattern_most_recent, origin_page):
-                return HttpResponseRedirect(reverse('questions') + '?sort_by=recent')
+                return HttpResponseRedirect(
+                    reverse('questions') + '?sort_by=recent'
+                )
             elif re.search(question_list_pattern_most_votes, origin_page):
-                return HttpResponseRedirect(reverse('questions') + '?sort_by=votes')
+                return HttpResponseRedirect(
+                    reverse('questions') + '?sort_by=votes'
+                )
 
             elif re.search(filtered_questions_pattern, origin_page):
-                return redirect('filter_by_tag', tag_slug=re.search(filtered_questions_pattern, origin_page).group('slug')) # testing passes from here
+                return redirect(
+                    'filter_by_tag', tag_slug=re.search(
+                        filtered_questions_pattern, origin_page).group('slug')
+                )
             elif re.search(question_list_pattern, origin_page):
-                return redirect('questions') 
+                return redirect('questions')
             elif re.search(question_detail_pattern, origin_page):
                 return redirect('question_detail', slug=obj.slug)
             else:
-                return redirect('questions') # default to questions page, shouldn't be necessary
+                # default to questions page in any rare cases,
+                # not expecting to use this line
+                return redirect('questions')
         elif isinstance(obj, Answer):
             return redirect('question_detail', slug=obj.question.slug)
 
-
-    def get(self, request, *args, **kwargs): 
-        """only allow POST requests. This is to prevent users from voting by directly accessing the URL"""
+    def get(self, request, *args, **kwargs):
+        """
+        only allow POST requests.
+        This is to prevent users from voting by directly accessing the URL
+        """
         return HttpResponseNotAllowed(['POST'])
+
 
 class QuestionUpvote(BaseVotingView):
     """ view for upvoting questions using the BaseVotingView class"""
     model = Question
     vote_type = 'upvotes'
 
+
 class QuestionDownvote(BaseVotingView):
     """ view for downvoting questions using the BaseVotingView class"""
     model = Question
     vote_type = 'downvotes'
+
 
 class AnswerUpvote(BaseVotingView):
     """ view for upvoting answers using the BaseVotingView class"""
     model = Answer
     vote_type = 'upvotes'
 
-    def get_redirect_url(self, obj, origin_page=None): 
+    def get_redirect_url(self, obj, origin_page=None):
         return redirect('question_detail', slug=obj.question.slug)
+
 
 class AnswerDownvote(BaseVotingView):
     """ view for downvoting answers using the BaseVotingView class """
