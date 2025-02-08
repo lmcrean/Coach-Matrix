1805793711a71baa43ab1b8c284a3e59184811bf
Laurie C <133490867+lmcrean@users.noreply.github.com>
2025-02-08 16:27:56 +0000

    Add unit test for Answer model creation and basic attributes

--- main_forum/tests/tests_answers.py
+++ main_forum/tests/tests_answers.py
@@ -1,21 +1,10 @@
-from django.test import TestCase, override_settings
+from django.test import TestCase
 from django.contrib.auth.models import User
 from main_forum.models.question_model import Question
-from django.utils.text import slugify
-from django_quill.fields import QuillField
-import os
+from main_forum.models.answer_model import Answer
 import json
 
-# Override database settings for testing
-@override_settings(
-    DATABASES={
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:'
-        }
-    }
-)
-class QuestionModelTest(TestCase):
+class SimpleAnswerTest(TestCase):
     def setUp(self):
         # Create a test user
         self.user = User.objects.create_user(
@@ -24,56 +13,44 @@ class QuestionModelTest(TestCase):
             password='testpass123'
         )
         
-        # Create a test question with QuillField content
+        # Create QuillField content for question
         quill_content = json.dumps({
-            'html': '<p>I need help understanding how to write effective unit tests in Django.</p>',
+            'html': '<p>This is a test question content</p>',
             'delta': {
                 'ops': [
-                    {'insert': 'I need help understanding how to write effective unit tests in Django.\n'}
+                    {'insert': 'This is a test question content\n'}
                 ]
             }
         })
         
-        # Create a test question
+        # Create a simple question
         self.question = Question.objects.create(
             author=self.user,
-            subject='How to write unit tests?',
+            subject='Test Question',
             content=quill_content,
             status=1  # Published
         )
-        # Add tags using taggit
-        self.question.tags.add('python', 'testing')
 
-    def test_question_creation(self):
-        """Test that a question can be created with proper attributes"""
-        self.assertEqual(self.question.subject, 'How to write unit tests?')
-        self.assertEqual(self.question.author, self.user)
-        self.assertEqual(
-            self.question.content.html,
-            '<p>I need help understanding how to write effective unit tests in Django.</p>'
-        )
-        self.assertEqual(self.question.status, 1)  # Published
-        self.assertEqual(self.question.slug, slugify('How to write unit tests?'))
-        self.assertIsNotNone(self.question.created_on)
-        self.assertEqual(str(self.question), 'How to write unit tests?')  # Testing __str__ method
-        self.assertEqual(sorted(list(self.question.tags.names())), sorted(['python', 'testing']))
+    def test_answer_creation(self):
+        """Test that we can create an answer for a question"""
+        # Create QuillField content for answer
+        answer_content = json.dumps({
+            'html': '<p>This is a test answer</p>',
+            'delta': {
+                'ops': [
+                    {'insert': 'This is a test answer\n'}
+                ]
+            }
+        })
         
-    def test_voting_system(self):
-        """Test the voting functionality"""
-        # Create another user to test voting
-        voter = User.objects.create_user(
-            username='voter',
-            email='voter@example.com',
-            password='voterpass123'
+        answer = Answer.objects.create(
+            author=self.user,
+            question=self.question,
+            body=answer_content,
+            status=1  # Published
         )
         
-        # Test upvoting
-        self.question.upvotes.add(voter)
-        self.assertEqual(self.question.number_of_upvotes(), 1)
-        
-        # Test downvoting
-        self.question.downvotes.add(voter)
-        self.assertEqual(self.question.number_of_downvotes(), 1)
-        
-        # Test net votes
-        self.assertEqual(self.question.net_votes, 0)  # Should be 0 as we have 1 upvote and 1 downvote
+        self.assertEqual(answer.author, self.user)
+        self.assertEqual(answer.question, self.question)
+        self.assertEqual(answer.status, 1)
+        self.assertTrue(answer.approved)  # Should be True by default
